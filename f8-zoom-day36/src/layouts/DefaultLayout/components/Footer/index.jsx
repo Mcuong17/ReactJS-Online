7768af7fa1c568634d917f21e821e6adaeb34119
Mcuong17 <manhcuong7381@gmail.com>
2025-09-25 07:10:33 +0700

    bo sung bai 36

--- f8-zoom-day36/src/layouts/DefaultLayout/components/Footer/index.jsx
+++ f8-zoom-day36/src/layouts/DefaultLayout/components/Footer/index.jsx
@@ -1,5 +1,11 @@
+
+import styles from './Footer.module.scss'
+
 function Footer() {
-    return <footer>Footer Default Layout</footer>
+    return <div className={styles.footer}>
+        <h1 className={styles.title}>Footer Default Layout</h1>
+        <span className={styles.subTitle}>@Coppy right by MCuong17</span>
+    </div>
 }
 
 export default Footer
